144791798b576ec9dc96f13aaf86dce5e88a7b90
jsnelgro <johnny.snelgrove@gmail.com>
2023-09-05 01:26:59 -0700

    comments/docs

--- processor/src/main/kotlin/jsnelgro/utility/type/annotations/Annotations.kt
+++ processor/src/main/kotlin/jsnelgro/utility/type/annotations/Annotations.kt
@@ -1,5 +1,8 @@
 package jsnelgro.utility.type.annotations
 
+/**
+ * Generate a new data class with the given [name], omitting any properties listed in [fields]
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 @Repeatable
@@ -8,6 +11,9 @@ annotation class Omit(
     val fields: Array<String>,
 )
 
+/**
+ * Generate a new data class with the given [name], containing only the properties listed in [fields]
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 @Repeatable
@@ -16,6 +22,12 @@ annotation class Pick(
     val fields: Array<String>,
 )
 
-// TODO: these are a little harder
+// TODO: these probably are a little harder to get right. Also questionable how useful they are.
+//  Can use Kotlin's sealed classes for Unions.
 //annotation class Union
+
+//  TODO: maybe Intersection is a better one to tackle next.
+//    Think you could pretty much just reuse Pick/Omit logic to dump all the fields from both into a new class
+//    e.g. @Intersection(ClassB::class, pick = [], omit = ["y"]) data class ClassA(val x: Int)
+//    pick and omit args are optional. probs Omit takes precedence to avoid accidental PII leakage?
 //annotation class Intersection
